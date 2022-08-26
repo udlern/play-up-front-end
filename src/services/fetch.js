328fa4c17055103e6b45677205c58a6d1bce96a8
Noa Udler <udlernoa4@gmail.com>
2022-08-26 11:59:10 -0700

    test

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -6,6 +6,8 @@ if (process.env.NODE_ENV !== "development") {
   backEndURL = "https://play-up-back-end.herokuapp.com";
 }
 
+console.log(backEndURL);
+
 export default function playUpFetch(url, configObj) {
   return fetch(backEndURL + url, configObj);
 }
